a968c92e04ea7e70241f757bcf3de827da1eeee8
Mariiya <58397107+Mariiya@users.noreply.github.com>
2020-12-04 00:56:14 +0200

    Update for  for Apartment, ApartmentSubBills models

--- src/main/java/com/netcracker/models/Apartment.java
+++ src/main/java/com/netcracker/models/Apartment.java
@@ -8,4 +8,15 @@ public class Apartment extends Account {
     private Integer squareMetres;
     private Integer floor;
     private Integer peopleCount;
+    Account account;
+
+    public Apartment(Integer apartmentNumber, Integer squareMetres, Integer floor, Integer peopleCount,Account account) {
+        this.apartmentNumber = apartmentNumber;
+        this.squareMetres = squareMetres;
+        this.floor = floor;
+        this.peopleCount = peopleCount;
+        this.account=account;
+    }
+
+    public Apartment(){}
 }
--- src/main/java/com/netcracker/models/ApartmentSubBill.java
+++ src/main/java/com/netcracker/models/ApartmentSubBill.java
@@ -9,5 +9,15 @@ public class ApartmentSubBill extends SubBill {
     private Double debt;
     private List<ApartmentOperation> apartmentOperation;
     private List<DebtPaymentOperation> debtPaymentOperation;
+    private SubBill subBill;
 
+    public ApartmentSubBill(Double debt, List<ApartmentOperation> apartmentOperation,
+                            List<DebtPaymentOperation> debtPaymentOperation, SubBill subBill) {
+        this.debt = debt;
+        this.apartmentOperation = apartmentOperation;
+        this.debtPaymentOperation = debtPaymentOperation;
+        this.subBill=subBill;
+    }
+
+    public ApartmentSubBill(){}
 }
