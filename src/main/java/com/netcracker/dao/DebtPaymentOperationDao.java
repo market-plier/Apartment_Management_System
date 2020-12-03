a6db930d855bbc5f47e75cb59a7ae2c78b8b3323
Anastasia Yakushyna <35917834+ayakushyna@users.noreply.github.com>
2020-12-03 15:51:05 +0200

    Fixed ManagerSpendingOperation class name

--- src/main/java/com/netcracker/dao/DebtPaymentOperationDao.java
+++ src/main/java/com/netcracker/dao/DebtPaymentOperationDao.java
@@ -1,13 +1,13 @@
 package com.netcracker.dao;
 
 import com.netcracker.models.DebtPaymentOperation;
-import com.netcracker.models.ManagerOperationSpending;
+import com.netcracker.models.ManagerSpendingOperation;
 
 import java.math.BigInteger;
 import java.util.List;
 
 public interface DebtPaymentOperationDao {
-    List<ManagerOperationSpending> getAllDebtPaymentOperations();
+    List<ManagerSpendingOperation> getAllDebtPaymentOperations();
 
     DebtPaymentOperation getDebtPaymentOperationById(BigInteger debtPaymentOperationId);
 
