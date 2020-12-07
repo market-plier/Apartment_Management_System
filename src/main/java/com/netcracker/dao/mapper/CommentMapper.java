c0e5169d7ce0764be4836a80c6dfae8c7cec137b
Anastasia Yakushyna <35917834+ayakushyna@users.noreply.github.com>
2020-12-07 20:14:49 +0200

    OD20DEVAT1-47 Fixed CommentMapper

--- src/main/java/com/netcracker/dao/mapper/CommentMapper.java
+++ src/main/java/com/netcracker/dao/mapper/CommentMapper.java
@@ -19,25 +19,21 @@ public class CommentMapper implements RowMapper<Comment> {
     @Override
     public Comment mapRow(ResultSet resultSet, int i) throws SQLException {
 
-        Comment comment = null;
-        try {
-            comment = new CommentBuilder()
-                    .witCommentId(new BigInteger(resultSet.getString("comment_id")))
-                    .withApartment(new ApartmentBuilder()
-                            .withApartmentId(new BigInteger(resultSet.getString("apartament_id")))
-                            .withApartmentFirstName(resultSet.getString("first_name"))
-                            .withApartmentLastName(resultSet.getString("last_name"))
-                            .withApartmentNumber(new BigInteger(resultSet.getString("apartament_number")))
-                            .build())
-                    .withBody(resultSet.getString("comment_body"))
-                    .withCreatedAt(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss:SSS").parse(resultSet.getString("created_at")))
-                    .withAnnouncement(new AnnouncementBuilder()
-                            .withAnnouncementId(new BigInteger(resultSet.getString("announcement_id")))
-                            .build())
-                    .build();
-        } catch (ParseException e) {
-            log.warn(e.getMessage(), e);
-        }
+        Comment comment = new CommentBuilder()
+                .witCommentId(new BigInteger(resultSet.getString("comment_id")))
+                .withApartment(new ApartmentBuilder()
+                        .withAccountId(new BigInteger(resultSet.getString("apartment_id")))
+                        .withFirstName(resultSet.getString("first_name"))
+                        .withLastName(resultSet.getString("last_name"))
+                        .withApartmentNumber(new Integer(resultSet.getString("apartament_number")))
+                        .build())
+                .withBody(resultSet.getString("comment_body"))
+                .withCreatedAt(resultSet.getDate("created_at"))
+                .withAnnouncement(new AnnouncementBuilder()
+                        .withAnnouncementId(new BigInteger(resultSet.getString("announcement_id")))
+                        .build())
+                .build();
+
         return comment;
     }
 }
