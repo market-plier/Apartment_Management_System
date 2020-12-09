a3c4c6d8aa87c7b7c202692060c89a0d3b6c29ef
Gleb Maksimenko <maksimenkogleb@gmail.com>
2020-12-09 21:39:42 +0200

    OD20DEVAT1-35 Update CommunalUtility POJO

--- src/main/java/com/netcracker/models/CommunalUtility.java
+++ src/main/java/com/netcracker/models/CommunalUtility.java
@@ -1,13 +1,27 @@
 package com.netcracker.models;
 
+import lombok.Data;
 import java.math.BigInteger;
-import java.time.Duration;
+import java.util.Date;
 
+@Data
 public class CommunalUtility {
+    public enum Status {ENABLE,DISABLE}
+    public enum Duration {TEMPORARY,CONSTANT}
     private BigInteger communalUtilityId;
     private CalculationMethod calculationMethod;
-  private String name;
+    private String name;
     private Duration durationType;
-   // private Status status;
-    //private Date deadline;
+    private Status status;
+    private Date deadline;
+
+    public CommunalUtility(BigInteger communalUtilityId, CalculationMethod calculationMethod,
+                           String name, Duration durationType, Status status, Date deadline) {
+        this.communalUtilityId = communalUtilityId;
+        this.calculationMethod = calculationMethod;
+        this.name = name;
+        this.durationType = durationType;
+        this.status = status;
+        this.deadline = deadline;
+    }
 }
