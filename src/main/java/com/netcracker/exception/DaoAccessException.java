db92dee798a921b0c22c2c29e69489223b7147a7
SashaBushanskiy <alexbushansky@gmail.com>
2020-12-10 16:59:49 +0200

    added ExceptionClass

---
+++ src/main/java/com/netcracker/exception/DaoAccessException.java
@@ -0,0 +1,26 @@
+package com.netcracker.exception;
+
+import org.springframework.dao.DataAccessException;
+
+import java.math.BigInteger;
+
+public class DaoAccessException extends DataAccessException {
+
+
+    public DaoAccessException(String msg) {
+        super(msg);
+    }
+
+    public DaoAccessException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public DaoAccessException(String msg, BigInteger id, Throwable cause) {
+        super(createMessage(msg, id), cause);
+    }
+
+    private static String createMessage(String msg, BigInteger id) {
+        return msg + " { " + id + " } ";
+    }
+
+}
