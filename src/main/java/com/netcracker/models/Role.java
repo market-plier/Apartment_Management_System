61f94240c4a508d6005c4ec382ab6f79b0685ee5
SashaBushanskiy <alexbushansky@gmail.com>
2020-12-11 19:06:12 +0200

    Added Role enum

--- src/main/java/com/netcracker/models/Role.java
+++ src/main/java/com/netcracker/models/Role.java
@@ -1,18 +1,10 @@
 package com.netcracker.models;
 
-import lombok.Data;
 
-import java.math.BigInteger;
 
-@Data
-public class Role {
-    protected BigInteger roleId;
-    protected String name;
-
-    public Role(BigInteger roleId, String roleName) {
-        this.roleId = roleId;
-        this.name = roleName;
-    }
+public enum Role {
+    MANAGER,
+    APARTMENT
 }
 
 
