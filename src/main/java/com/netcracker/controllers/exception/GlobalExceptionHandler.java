d0d36b30b21bde22ce03a1beede86a8ad4a16ac0
SashaBushanskiy <alexbushansky@gmail.com>
2020-12-20 12:58:18 +0200

    added GlobalExceptionHandler.java

---
+++ src/main/java/com/netcracker/controllers/exception/GlobalExceptionHandler.java
@@ -0,0 +1,100 @@
+package com.netcracker.controllers.exception;
+
+import com.netcracker.controllers.web.ApiError;
+import com.netcracker.controllers.web.ResponseEntityBuilder;
+import com.netcracker.exception.DaoAccessException;
+import com.netcracker.exception.NotBelongToAccountException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> details = new ArrayList<String>();
+        details = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " : " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiError err = new ApiError(LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST,
+                "Validation Errors",
+                details);
+
+        return ResponseEntityBuilder.build(err);
+    }
+
+
+    @ExceptionHandler({DaoAccessException.class, NotBelongToAccountException.class, NullPointerException.class})
+    public final ResponseEntity<ApiError> handleCustomException(Exception ex, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+
+        if (ex instanceof DaoAccessException) {
+            HttpStatus status = HttpStatus.NOT_FOUND;
+            DaoAccessException accessException = (DaoAccessException) ex;
+
+            return handleDaoAccessException(accessException, headers, status, request);
+        } else if (ex instanceof NotBelongToAccountException) {
+            HttpStatus status = HttpStatus.BAD_REQUEST;
+            NotBelongToAccountException accountException = (NotBelongToAccountException) ex;
+
+            return handleNotBelongToAccountException(accountException, headers, status, request);
+        } else if (ex instanceof NullPointerException) {
+            HttpStatus status = HttpStatus.BAD_REQUEST;
+            NullPointerException nullPointerException = (NullPointerException) ex;
+            return handleNullPointerException(nullPointerException, headers, status, request);
+        } else {
+            HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+            return handleExceptionInternal(ex, null, headers, status, request);
+        }
+    }
+
+
+    protected ResponseEntity<ApiError> handleDaoAccessException(DaoAccessException ex, HttpHeaders headers,
+                                                                HttpStatus status, WebRequest request) {
+        List<String> errors = Collections.singletonList(ex.getMessage());
+
+        return handleExceptionInternal(ex, new ApiError(LocalDateTime.now(), status, "NOT BELONG TO USER", errors), headers, status, request);
+    }
+
+
+    protected ResponseEntity<ApiError> handleNullPointerException(NullPointerException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+        List<String> errors = Collections.singletonList(ex.getMessage());
+        return handleExceptionInternal(ex, new ApiError(LocalDateTime.now(), status, "NULL POINTER", errors), headers, status, request);
+    }
+
+    protected ResponseEntity<ApiError> handleNotBelongToAccountException(NotBelongToAccountException ex,
+                                                                         HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = Collections.singletonList(ex.getMessage());
+        return handleExceptionInternal(ex, new ApiError(LocalDateTime.now(), status, "DAO exception", errors), headers, status, request);
+    }
+
+
+    protected ResponseEntity<ApiError> handleExceptionInternal(Exception ex, ApiError body, HttpHeaders headers,
+                                                               HttpStatus status, WebRequest request) {
+
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+        }
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
