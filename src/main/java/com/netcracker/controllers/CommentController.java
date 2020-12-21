7e21bdc9d3e0af76e168d253f9a5a1dd294e0610
SashaBushanskiy <alexbushansky@gmail.com>
2020-12-21 15:46:31 +0200

    OD20DEVAT1-84 fix

--- src/main/java/com/netcracker/controllers/CommentController.java
+++ src/main/java/com/netcracker/controllers/CommentController.java
@@ -2,12 +2,10 @@ package com.netcracker.controllers;
 
 
 import com.netcracker.exception.DaoAccessException;
-import com.netcracker.exception.NotBelongToAccountException;
 import com.netcracker.models.Comment;
 import com.netcracker.secutity.jwt.JwtAccount;
 import com.netcracker.services.CommentService;
 import lombok.extern.slf4j.Slf4j;
-import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,35 +33,24 @@ public class CommentController {
     @RequestMapping(method = RequestMethod.POST)
     @PreAuthorize("hasAnyRole('ROLE_OWNER')")
     public ResponseEntity create(@RequestBody @Valid Comment comment, @AuthenticationPrincipal JwtAccount account) throws NullPointerException, DaoAccessException {
-
-
             commentService.createComment(comment, account.getId());
-
              return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @RequestMapping(value = "{commentId}", method = RequestMethod.DELETE)
     @PreAuthorize("hasAnyRole('ROLE_OWNER')")
     public ResponseEntity deleteComment(@AuthenticationPrincipal JwtAccount account, @PathVariable String commentId) {
-        try {
+
 
             commentService.deleteComment(new BigInteger(commentId), account.getId());
-        } catch (NullPointerException | DaoAccessException | NotBelongToAccountException e) {
-            log.error(e.getMessage(), e);
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return ResponseEntity.ok(HttpStatus.OK);
+            return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     @PreAuthorize("hasAnyRole('ROLE_OWNER')")
     public ResponseEntity updateComment(@AuthenticationPrincipal JwtAccount account, @RequestBody @Valid Comment comment) {
-        try {
+
             commentService.updateComment(comment, account.getId());
-        } catch (NullPointerException | DaoAccessException | NotBelongToAccountException e) {
-            log.error(e.getMessage(), e);
-            return new ResponseEntity<>(new JSONObject().put("error",e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-        return ResponseEntity.ok(HttpStatus.OK);
+             return ResponseEntity.ok(HttpStatus.OK);
     }
 }
