ca1600cbe00607212042a94c8a239096552478be
BPodhorny <75379258+BPodhorny@users.noreply.github.com>
2020-12-21 17:56:04 +0200

    DebtPaymentOperationService build rabotaet

--- src/main/java/com/netcracker/services/DebtPaymentOperationService.java
+++ src/main/java/com/netcracker/services/DebtPaymentOperationService.java
@@ -30,62 +30,31 @@ public class DebtPaymentOperationService {
         this.managerSubBillService = managerSubBillService;
     }
 
-    public List<DebtPaymentOperation> getDebtPaymentOperationsByApartmentId(BigInteger apartmentId) throws DataNotFoundException {
-        try {
-            return debtPaymentOperationDao.getDebtPaymentOperationsByApartmentId(apartmentId);
-        } catch (DataAccessException e) {
-            log.warn("Error getting debt payment operations by apartment_id = " + apartmentId + " :", e);
-            throw new DataNotFoundException(e.getMessage(), ErrorCodes._FAIL_TO_SELECT);
-        }
+    public List<DebtPaymentOperation> getDebtPaymentOperationsByApartmentId(BigInteger apartmentId) {
+        return debtPaymentOperationDao.getDebtPaymentOperationsByApartmentId(apartmentId);
     }
 
-    public List<DebtPaymentOperation> getDebtPaymentOperationsByApartmentSubBillId(BigInteger apartmentSubBillId) throws DataNotFoundException {
-        try {
-            return debtPaymentOperationDao.getDebtPaymentOperationsByApartmentSubBillId(apartmentSubBillId);
-        } catch (DataAccessException e) {
-            log.warn("Error getting debt payment operations by apartment_sub_bill_id = " + apartmentSubBillId + " :", e);
-            throw new DataNotFoundException(e.getMessage(), ErrorCodes._FAIL_TO_SELECT);
-        }
+    public List<DebtPaymentOperation> getDebtPaymentOperationsByApartmentSubBillId(BigInteger apartmentSubBillId){
+        return debtPaymentOperationDao.getDebtPaymentOperationsByApartmentSubBillId(apartmentSubBillId);
     }
 
-    public List<DebtPaymentOperation> getDebtPaymentOperationsByManagerSubBillId(BigInteger managerSubBilId) throws DataNotFoundException {
-        try {
-            return debtPaymentOperationDao.getDebtPaymentOperationsByManagerSubBillId(managerSubBilId);
-        } catch (DataAccessException e) {
-            log.warn("Error getting debt payment operations by manager_sub_bill_id = " + managerSubBilId + " :", e);
-            throw new DataNotFoundException(e.getMessage(), ErrorCodes._FAIL_TO_SELECT);
-        }
+    public List<DebtPaymentOperation> getDebtPaymentOperationsByManagerSubBillId(BigInteger managerSubBilId) {
+        return debtPaymentOperationDao.getDebtPaymentOperationsByManagerSubBillId(managerSubBilId);
     }
 
-    public List<DebtPaymentOperation> getDebtPaymentOperationsByDateRangeAndApartmentId(Date from, Date to, BigInteger apartmentId) throws DataNotFoundException {
-        try {
-            return debtPaymentOperationDao.getDebtPaymentOperationsByDateRangeAndApartmentId(from, to, apartmentId);
-        } catch (DataAccessException e) {
-            log.warn("Error getting debt payment operations by date range from " + from + " to " + to + " and apartment_id = " + apartmentId + " :", e);
-            throw new DataNotFoundException(e.getMessage(), ErrorCodes._FAIL_TO_SELECT);
-        }
+    public List<DebtPaymentOperation> getDebtPaymentOperationsByDateRangeAndApartmentId(Date from, Date to, BigInteger apartmentId) {
+        return debtPaymentOperationDao.getDebtPaymentOperationsByDateRangeAndApartmentId(from, to, apartmentId);
     }
 
-    public List<DebtPaymentOperation> getDebtPaymentOperationsByDateRangeAndApartmentSubBillId(Date from, Date to, BigInteger apartmentSubBillId) throws DataNotFoundException {
-        try {
-            return debtPaymentOperationDao.getDebtPaymentOperationsByDateRangeAndApartmentSubBillId(from, to, apartmentSubBillId);
-        } catch (DataAccessException e) {
-            log.warn("Error getting debt payment operations by date range from " + from + " to " + to + " and apartment_sub_bill_id = " + apartmentSubBillId + " :", e);
-            throw new DataNotFoundException(e.getMessage(), ErrorCodes._FAIL_TO_SELECT);
-        }
+    public List<DebtPaymentOperation> getDebtPaymentOperationsByDateRangeAndApartmentSubBillId(Date from, Date to, BigInteger apartmentSubBillId) {
+        return debtPaymentOperationDao.getDebtPaymentOperationsByDateRangeAndApartmentSubBillId(from, to, apartmentSubBillId);
     }
 
-    public List<DebtPaymentOperation> getDebtPaymentOperationsByDateRangeAndManagerSubBillId(Date from, Date to, BigInteger managerSubBillId) throws DataNotFoundException {
-        try {
-            return debtPaymentOperationDao.getDebtPaymentOperationsByDateRangeAndManagerSubBillId(from, to, managerSubBillId);
-        } catch (DataAccessException e) {
-            log.warn("Error getting debt payment operations by date range from " + from + " to " + to + " and manager_sub_bill_id = " + managerSubBillId + " :", e);
-            throw new DataNotFoundException(e.getMessage(), ErrorCodes._FAIL_TO_SELECT);
-        }
+    public List<DebtPaymentOperation> getDebtPaymentOperationsByDateRangeAndManagerSubBillId(Date from, Date to, BigInteger managerSubBillId) {
+        return debtPaymentOperationDao.getDebtPaymentOperationsByDateRangeAndManagerSubBillId(from, to, managerSubBillId);
     }
 
-    public void createDebtPaymentOperation(ApartmentSubBill apartmentSubBill, Double sum) throws CreationException {
-        try {
+    public void createDebtPaymentOperation(ApartmentSubBill apartmentSubBill, Double sum) {
             ManagerSubBill managerSubBill = managerSubBillService.getManagerSubBillByCommunalUtilityId(apartmentSubBill.getCommunalUtility().getCommunalUtilityId());
             managerSubBill.setBalance(managerSubBill.getBalance() + sum);
 
@@ -95,9 +64,5 @@ public class DebtPaymentOperationService {
                     .withManagerSubBill(managerSubBill)
                     .withSum(sum)
                     .build());
-        } catch (DataAccessException e) {
-            log.warn("Error creating debt payment operation with apartment_sub_bill = " + apartmentSubBill + " :", e);
-            throw new CreationException(e.getMessage(), ErrorCodes._FAIL_TO_INSERT);
-        }
     }
 }
