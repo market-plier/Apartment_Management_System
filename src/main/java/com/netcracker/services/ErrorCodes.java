a9f2f3810c7cd993d473fc5a916af36ed4df76cc
BPodhorny <75379258+BPodhorny@users.noreply.github.com>
2020-12-21 17:12:08 +0200

    OD20DEVAT1-39, OD20DEVAT1-42 Created ApartmentOperationBuilder

---
+++ src/main/java/com/netcracker/services/ErrorCodes.java
@@ -0,0 +1,9 @@
+package com.netcracker.services;
+
+import java.math.BigInteger;
+
+public interface ErrorCodes {
+    BigInteger _FAIL_TO_INSERT = BigInteger.valueOf(1);
+    BigInteger _FAIL_TO_UPDATE = BigInteger.valueOf(2);
+    BigInteger _FAIL_TO_SELECT = BigInteger.valueOf(3);
+}
