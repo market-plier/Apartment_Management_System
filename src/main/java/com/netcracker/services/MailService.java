6d9500a50f97ac29681f0b6518b2770a3f7a4438
Anastasia Yakushyna <35917834+ayakushyna@users.noreply.github.com>
2020-12-21 15:23:02 +0200

    OD20DEVAT1-66 Added MailConfig and MailService

---
+++ src/main/java/com/netcracker/services/MailService.java
@@ -0,0 +1,23 @@
+package com.netcracker.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendMessage(String to, String subject, String text) throws MailException {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@baeldung.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
