1805c2a0e6766fa8390fd21dd53bff97158c3277
Anastasia Yakushyna <35917834+ayakushyna@users.noreply.github.com>
2020-12-22 16:28:48 +0200

    OD20DEVAT1-66 Added sendMessageWithAttachment method

--- src/main/java/com/netcracker/services/MailService.java
+++ src/main/java/com/netcracker/services/MailService.java
@@ -4,7 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 
 @Service
@@ -18,6 +29,30 @@ public class MailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+        emailSender.send(message);
+    }
+
+    public void sendMessageWithAttachment(String to, String subject, String text, String filename, ByteArrayInputStream stream)
+            throws MessagingException, IOException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("noreply@baeldung.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+
+        //construct the pdf body part
+        DataSource dataSource = new ByteArrayDataSource(stream, "application/pdf");
+        MimeBodyPart attachment = new MimeBodyPart();
+        attachment.setDataHandler(new DataHandler(dataSource));
+        attachment.setFileName(filename);
+
+        //construct the mime multi part
+        MimeMultipart mimeMultipart = new MimeMultipart();
+        mimeMultipart.addBodyPart(attachment);
+
+        message.setContent(mimeMultipart);
         emailSender.send(message);
     }
 }
