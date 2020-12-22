e3ebab0188614801c940200fe216cd518a3587e6
BPodhorny <75379258+BPodhorny@users.noreply.github.com>
2020-12-22 17:01:57 +0200

    build rabotaet?!

--- src/main/java/com/netcracker/exception/DaoAccessException.java
+++ src/main/java/com/netcracker/exception/DaoAccessException.java
@@ -8,9 +8,32 @@ public class DaoAccessException extends DataAccessException {
 
     private BigInteger errorMessage;
 
+    public DaoAccessException(String msg) {
+        super(msg);
+    }
+
+    public DaoAccessException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
     public DaoAccessException(String msg,  BigInteger id, Throwable cause, BigInteger errorMessage) {
         super(createMessage(msg, id), cause);
         this.errorMessage = errorMessage;
+    }
+
+    public DaoAccessException(String msg,  BigInteger id, BigInteger errorMessage) {
+        super(createMessage(msg, id));
+        this.errorMessage = errorMessage;
+    }
+
+
+    public DaoAccessException(String msg,  BigInteger errorMessage) {
+        super(msg);
+        this.errorMessage = errorMessage;
+    }
+
+    public DaoAccessException(String msg, BigInteger id, Throwable cause) {
+        super(createMessage(msg, id), cause);
     }
 
     private static String createMessage(String msg, BigInteger id) {
