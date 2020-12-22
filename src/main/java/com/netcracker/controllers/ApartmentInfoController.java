924d317e5852d4cd62ccfac0a74e2348f9f8a2a8
Mariiya <58397107+Mariiya@users.noreply.github.com>
2020-12-22 15:24:25 +0200

    Fixing build

--- src/main/java/com/netcracker/controllers/ApartmentInfoController.java
+++ src/main/java/com/netcracker/controllers/ApartmentInfoController.java
@@ -1,34 +1,63 @@
 package com.netcracker.controllers;
 
+import com.netcracker.models.Account;
 import com.netcracker.models.Apartment;
+import com.netcracker.models.PojoBuilder.AccountBuilder;
+import com.netcracker.models.Role;
+import com.netcracker.secutity.jwt.JwtAccount;
 import com.netcracker.services.ApartmentInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.math.BigInteger;
 import java.util.List;
 
 @RestController
 public class ApartmentInfoController {
-    @Autowired
-    private ApartmentInfoService apartmentInfoService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/createApartment")
-    public Apartment createApartment(@RequestBody Apartment apartment) {
+    private final ApartmentInfoService apartmentInfoService;
+
+    @Autowired
+    public ApartmentInfoController(ApartmentInfoService apartmentInfoService) {
+        this.apartmentInfoService = apartmentInfoService;
+    }
+
+    @PostMapping("/createApartment")
+    @PreAuthorize("hasAnyRole('ROLE_MANAGER')")
+    public Apartment createApartment(@RequestBody @Valid Apartment apartment) {
         return apartmentInfoService.createApartment(apartment);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/updateApartment")
-    public Apartment updateApartment(@RequestBody Apartment apartment) {
-        return apartmentInfoService.updateApartment(apartment);
+    @PutMapping("/updateApartment")
+    @PreAuthorize("hasAnyRole('ROLE_MANAGER','ROLE_OWNER')")
+    public Apartment updateApartment(@RequestBody @Valid Apartment apartment) {
+        JwtAccount account = (JwtAccount) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Role role = null;
+        if (account.getAuthorities().toString().equals("[ROLE_OWNER]")) {
+            role = Role.OWNER;
+        }
+        if (account.getAuthorities().toString().equals("[ROLE_MANAGER]")) {
+            role = Role.MANAGER;
+        }
+        Account updater = new AccountBuilder()
+                .withAccountId(account.getId())
+                .withRole(role)
+                .build();
+        return apartmentInfoService.updateApartment(apartment, updater);
     }
 
-    @RequestMapping("/getApartment/{id}")
-    public Apartment getApartment(@PathVariable BigInteger id) {
+    @PreAuthorize("hasAnyRole('ROLE_MANAGER','ROLE_OWNER')")
+    @GetMapping("/getApartment/{id}")
+    public Apartment getApartment(@PathVariable @NotNull BigInteger id) {
         return apartmentInfoService.getApartmentById(id);
     }
 
-    @RequestMapping("/getAllApartments")
+    @PreAuthorize("hasAnyRole('ROLE_MANAGER')")
+    @GetMapping("/getAllApartments")
     public List<Apartment> getAllApartments() {
         return apartmentInfoService.getAllApartments();
     }
