9db78e0cb28905bfef8134c93ed9e2b994ae469a
SashaBushanskiy <alexbushansky@gmail.com>
2020-12-22 20:16:27 +0200

    fix GlobalExceptionHandler

--- src/main/java/com/netcracker/controllers/exception/GlobalExceptionHandler.java
+++ src/main/java/com/netcracker/controllers/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.netcracker.controllers.web.ApiError;
 import com.netcracker.controllers.web.ResponseEntityBuilder;
 import com.netcracker.exception.DaoAccessException;
 import com.netcracker.exception.NotBelongToAccountException;
+import com.netcracker.secutity.jwt.JwtAuthenticationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.util.WebUtils;
 
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -43,8 +45,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntityBuilder.build(err);
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException exception) {
 
-    @ExceptionHandler({DaoAccessException.class, NotBelongToAccountException.class, NullPointerException.class})
+            List<String> details = exception.getConstraintViolations().stream().map(error -> error.getMessage()).collect(Collectors.toList());
+            ApiError err = new ApiError(LocalDateTime.now(),
+                    HttpStatus.BAD_REQUEST,
+                    "Validation Errors",
+                    details);
+            return ResponseEntityBuilder.build(err);
+
+    }
+
+
+
+    @ExceptionHandler({DaoAccessException.class, NotBelongToAccountException.class, NullPointerException.class, JwtAuthenticationException.class})
     public final ResponseEntity<ApiError> handleCustomException(Exception ex, WebRequest request) {
         HttpHeaders headers = new HttpHeaders();
 
@@ -62,7 +77,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             HttpStatus status = HttpStatus.BAD_REQUEST;
             NullPointerException nullPointerException = (NullPointerException) ex;
             return handleNullPointerException(nullPointerException, headers, status, request);
-        } else {
+        } else if (ex instanceof JwtAuthenticationException) {
+            HttpStatus status = HttpStatus.FORBIDDEN;
+            JwtAuthenticationException authenticationException = (JwtAuthenticationException) ex;
+            return handleJwtAccessException(authenticationException,headers,status,request);
+        }
+        else {
             HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
             return handleExceptionInternal(ex, null, headers, status, request);
         }
@@ -74,6 +94,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         List<String> errors = Collections.singletonList(ex.getMessage());
 
         return handleExceptionInternal(ex, new ApiError(LocalDateTime.now(), status, "NOT BELONG TO USER", errors), headers, status, request);
+    }
+
+
+    protected ResponseEntity<ApiError> handleJwtAccessException(JwtAuthenticationException ex, HttpHeaders headers,
+                                                                HttpStatus status, WebRequest request) {
+        List<String> errors = Collections.singletonList(ex.getMessage());
+        return handleExceptionInternal(ex, new ApiError(LocalDateTime.now(), status, "TOKEN IS WRONG OR INSPIRED", errors), headers, status, request);
     }
 
 
