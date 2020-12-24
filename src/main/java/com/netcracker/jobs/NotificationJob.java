7eae0c20fdbf1ce1773155ccf135c0283631be09
Anastasia Yakushyna <35917834+ayakushyna@users.noreply.github.com>
2020-12-24 21:42:29 +0200

    OD20DEVAT1-85 Added NotificationJob

---
+++ src/main/java/com/netcracker/jobs/NotificationJob.java
@@ -0,0 +1,47 @@
+package com.netcracker.jobs;
+
+import com.netcracker.exception.DaoAccessException;
+import com.netcracker.services.NotificationService;
+import lombok.extern.log4j.Log4j;
+import org.apache.log4j.Level;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.Date;
+
+@Log4j
+@Component
+public class NotificationJob implements Job {
+    private final Trigger trigger;
+
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationJob(NotificationService notificationService) {
+        trigger = new CronTrigger("0 0 9 1 1/1 *");   //Every first day of month
+        this.notificationService = notificationService;
+    }
+
+    @Override
+    public Runnable getJob() {
+        return () -> {
+            try {
+                notificationService.sendDebtNotificationToAllApartments();
+            } catch (IOException | MessagingException | DaoAccessException | NullPointerException e) {
+                log.error("NotificationJob method getJob: " + e.getMessage(), e);
+            }
+
+            log.log(Level.INFO, new Date() + " NotificationJob with "
+                    + " on thread " + Thread.currentThread().getName());
+        };
+    }
+
+    @Override
+    public Trigger getTrigger() {
+        return trigger;
+    }
+}
