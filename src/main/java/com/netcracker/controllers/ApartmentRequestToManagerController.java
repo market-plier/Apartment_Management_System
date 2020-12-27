07925a4af6e84e39e5e12d01de97af1ae87c9e61
Mariiya <58397107+Mariiya@users.noreply.github.com>
2020-12-27 16:15:07 +0200

    message added to empty request

--- src/main/java/com/netcracker/controllers/ApartmentRequestToManagerController.java
+++ src/main/java/com/netcracker/controllers/ApartmentRequestToManagerController.java
@@ -13,6 +13,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.math.BigInteger;
 
 @RestController
@@ -28,11 +30,18 @@ public class ApartmentRequestToManagerController {
     @RequestMapping(method = RequestMethod.POST, value = "/createApartmentRequestToManager")
     @PreAuthorize("hasAnyRole('ROLE_OWNER')")
     public ResponseEntity<String> createApartmentRequestToManager(@RequestBody String text) {
+
+        if (text.trim().isEmpty()) {
+            throw new IllegalArgumentException("Request text can not be empty");
+        }
+
         JwtAccount account = (JwtAccount) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         ApartmentRequestToManager request = new ApartmentRequestToManager(account.getId(), text);
+
         if (account.getId().equals(request.getApartmentId())) {
             apartmentRequestToManagerService.generateApartmentRequestToManager(request);
             return new ResponseEntity<>("Request is successfully sended", HttpStatus.OK);
+
         } else {
             NotBelongToAccountException e = new NotBelongToAccountException("Can not send request from this account");
             log.log(Level.WARN, e.getMessage(), e);
