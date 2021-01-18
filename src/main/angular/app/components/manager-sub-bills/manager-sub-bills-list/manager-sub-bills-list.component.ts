78002b963c8d67a6bb8afa97e08610a34c99ae11
FlokiOne <47519410+FlokiOne@users.noreply.github.com>
2021-01-18 08:26:34 +0200

    some fix

--- src/main/angular/app/components/manager-sub-bills/manager-sub-bills-list/manager-sub-bills-list.component.ts
+++ src/main/angular/app/components/manager-sub-bills/manager-sub-bills-list/manager-sub-bills-list.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {ManagerSubBill} from "../../../models/manager-sub-bill";
-import {Router} from "@angular/router";
+import {Data, Router} from "@angular/router";
 import {ManagerSubBillService} from "../../../services/manager-sub-bill.service";
-import {Observable, of} from "rxjs";
-import {map, startWith} from "rxjs/operators";
-import {FormControl} from "@angular/forms";
 import {MatTableDataSource} from "@angular/material/table";
 import {CommunalUtility} from "../../../models/communal-utility";
 import {TokenStorageService} from "../../../services/token-storage.service";
+import {Sort} from '@angular/material/sort';
+
+
+export interface Dessert {
+    idSort: number;
+    nameSort: string;
+    statusSort: string;
+    calculationSort: string;
+    balanceSort: number;
+}
+
+function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
 
 @Component({
   selector: 'app-manager-sub-bills-list',
@@ -15,12 +26,14 @@ import {TokenStorageService} from "../../../services/token-storage.service";
   styleUrls: ['./manager-sub-bills-list.component.scss']
 })
 
+
 export class ManagerSubBillListComponent implements OnInit {
 
   managerSubBills?: ManagerSubBill[] = [];
   datasource: MatTableDataSource<ManagerSubBill>
   communal: CommunalUtility[];
-  displayedColumns: string[] = ['name', 'status', 'deadline', 'calculation', 'balance'];
+  sortedData: ManagerSubBill[];
+  displayedColumns: string[] = ['id', 'name', 'status', 'deadline', 'calculation', 'balance'];
 
   constructor(private managerSubBillService: ManagerSubBillService,
               private tokenStorageService: TokenStorageService,
@@ -58,6 +71,28 @@ export class ManagerSubBillListComponent implements OnInit {
 
     getRole(): any {
         return this.tokenStorageService.getRole();
+    }
+
+    sortData(sort: Sort) {
+        const data = this.managerSubBills.slice();
+        if (!sort.active || sort.direction === '') {
+            this.sortedData = data;
+            return;
+        }
+
+        this.sortedData = data.sort((a, b) => {
+            const isAsc = sort.direction === 'asc';
+            switch (sort.active) {
+                case 'idSort': return compare(a.subBillId, b.subBillId, isAsc);
+                case 'statusSort': return compare(a.communalUtility.status, b.communalUtility.status, isAsc);
+                case 'balanceSort': return compare(a.balance, b.balance, isAsc);
+                case 'deadlineSort': return compare(a.communalUtility.deadline, b.communalUtility.deadline, isAsc);
+                case 'nameSort': return compare(a.communalUtility.name, a.communalUtility.name, isAsc);
+                case 'calculationSort': return compare(a.communalUtility.calculationMethod, a.communalUtility.calculationMethod, isAsc);
+                default: return 0;
+            }
+
+        });
     }
 
 }
