2868aea7176180257a7ad035e818db135f52c32b
BPodhorny <75379258+BPodhorny@users.noreply.github.com>
2021-01-23 19:30:24 +0200

    init

--- src/main/angular/environments/environment.prod.ts
+++ src/main/angular/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: true,
-  api: 'https://house-management-angular.herokuapp.com/',
-  redirectUrl: 'https://house-management-angular.herokuapp.com//implicit/callback'
 };
