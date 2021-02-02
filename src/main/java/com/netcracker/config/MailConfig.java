67f251706ee48c7c275bcb1d8e8bac70c0294b22
Mariiya <58397107+Mariiya@users.noreply.github.com>
2021-02-02 20:32:54 +0200

    mail config

--- src/main/java/com/netcracker/config/MailConfig.java
+++ src/main/java/com/netcracker/config/MailConfig.java
@@ -28,6 +28,8 @@ public class MailConfig {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
+        props.put("mail.smtp.starttls.required","true");
+        props.put("mail.smtp.socketFactory.fallback","true");
 
         return mailSender;
     }
